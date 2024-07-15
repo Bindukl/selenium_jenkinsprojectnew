f3643bc4286553b068ab0d67dccab606971e4513
Bindu <bindulokesh9@gmail.com>
2024-07-15 10:00:11 +0530

    secand push

---
+++ COM.Qsp_JenkinsJob/src/test/java/jenkinsprojectnew/SecandProjectTest.java
@@ -0,0 +1,12 @@
+package jenkinsprojectnew;
+
+import org.testng.Reporter;
+
+public class SecandProjectTest {
+	
+	public void build()
+	{
+		Reporter.log("Execute the bulid successfully");
+	}
+
+}
