0407b0a901c467a7a1d2ccfe292644394573deb3
Bindu <bindulokesh9@gmail.com>
2024-07-15 09:37:53 +0530

    first push

---
+++ COM.Qsp_JenkinsJob/src/test/java/jenkinsprojectnew/NewDemoProjectTest.java
@@ -0,0 +1,14 @@
+package jenkinsprojectnew;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class NewDemoProjectTest {
+	
+	@Test
+	public void execute()
+	{
+		Reporter.log("Jenkins class executed successfully",true);
+	}
+
+}
