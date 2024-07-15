2df954d2aa6ec54bd32bc8840e0e18bf533e9ee1
Bindu <bindulokesh9@gmail.com>
2024-07-15 10:12:04 +0530

    second push

--- COM.Qsp_JenkinsJob/src/test/java/jenkinsprojectnew/SecondProjectTest.java
+++ COM.Qsp_JenkinsJob/src/test/java/jenkinsprojectnew/SecondProjectTest.java
@@ -1,9 +1,11 @@
 package jenkinsprojectnew;
 
 import org.testng.Reporter;
+import org.testng.annotations.Test;
 
-public class SecandProjectTest {
+public class SecondProjectTest {
 	
+	@Test
 	public void build()
 	{
 		Reporter.log("Execute the bulid successfully");
