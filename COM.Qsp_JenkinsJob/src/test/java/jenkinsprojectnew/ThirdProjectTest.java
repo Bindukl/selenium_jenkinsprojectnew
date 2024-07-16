dd48af7abb9b2b3e673b6fd57ee914e6ea356658
Bindu <bindulokesh9@gmail.com>
2024-07-16 09:54:37 +0530

    third push

---
+++ COM.Qsp_JenkinsJob/src/test/java/jenkinsprojectnew/ThirdProjectTest.java
@@ -0,0 +1,14 @@
+package jenkinsprojectnew;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ThirdProjectTest {
+	
+	@Test
+	public void buildThree()
+	{
+		Reporter.log("Our third Build successfully lunched");
+	}
+
+}
